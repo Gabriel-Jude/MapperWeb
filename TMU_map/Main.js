bc1424ad60640891af199ab79f89bafdc00a4972
Gabriel Jude <92489682+Gabriel-Jude@users.noreply.github.com>
2022-12-06 21:42:17 -0500

    Drop Down Menu

--- TMU_map/Main.js
+++ TMU_map/Main.js
@@ -12,6 +12,45 @@ var mapOptions = {
 //create map
 var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
+const marker = new google.maps.Marker({
+    position: { lat: 43.65882493196989, lng: -79.3802579882013 },
+    map: map,
+    title: "Kerr Hall",
+    label: "Kerr Hall",
+    icon: {
+        url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        Size: new google.maps.Size(15, 15),
+    }
+  });
+
+
+
+// // Change markers on zoom
+
+//   map.addListener('zoom_changed', function () {
+//     var zoom = map.getZoom();
+//     console.log(zoom);
+//     // iterate over markers and call setVisible
+//     for (var i = 0; i < markerArray.length; i++) {
+//       // Change markers on zoom
+//       console.log('marker' + markerArray[i])
+
+//       if (map.getZoom() >= 4) {
+    
+//         if (map.getZoom() == 4) {
+//           markerArray[i].setIcon(icons[features[i].type].medZoomIcon);
+//           markerArray[i].setAnimation(google.maps.Animation.DROP);
+//           markerArray[i].setVisible(true);
+//         }
+//       } else {
+//         markerArray[i].setIcon(icons[features[i].type].highZoomIcon);
+//       }
+//     }
+//   });
+
+
 //create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
 
@@ -38,7 +77,7 @@ function calcRoute() {
 
             //Get distance and time
             const output = document.querySelector('#output');
-            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Walking distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
 
             //display route
             directionsDisplay.setDirections(result);
@@ -49,7 +88,7 @@ function calcRoute() {
             map.setCenter(myLatLng);
 
             //show error message
-            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
+            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve Walking distance.</div>";
         }
     });
 
@@ -65,10 +104,10 @@ var options = {
     componentRestrictions: {country: "CA",}
 }
 
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
+var autocomplete1 = document.getElementById("from");
+// var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 autocomplete1.bindTo("bounds",map);
 
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+var autocomplete2 = document.getElementById("to");
+// var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 autocomplete2.bindTo("bounds",map);
